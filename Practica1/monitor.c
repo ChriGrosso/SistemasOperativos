c7df48093846dcb5d1e39cc22ef78c631db901e2
pablopc3 <130652177+pablopc3@users.noreply.github.com>
2025-02-24 11:49:41 +0100

    Update monitor.c

--- Practica1/monitor.c
+++ Practica1/monitor.c
@@ -3,25 +3,27 @@
 #include <unistd.h>
 #include "pow.h"
 
-// El Monitor lee los pares (target, solución), valida la solución y
-// envía una confirmación de vuelta al Minero.
-void monitor(int pipe_m2mon_read, int pipe_mon2m_write) {
+void monitor(int pipe1_read, int pipe2_write) {
+
     long int target, solution;
-    int confirmation;
-    while (read(pipe_m2mon_read, &target, sizeof(long int)) > 0) {
-        if (read(pipe_m2mon_read, &solution, sizeof(long int)) != sizeof(long int)) {
-            perror("Error leyendo solución en monitor");
+    int check;
+    
+    while (read(pipe1_read, &target, sizeof(long int)) > 0) {
+        if (read(pipe1_read, &solution, sizeof(long int)) != sizeof(long int)) {
+            perror("Error reading solution");
             exit(EXIT_FAILURE);
         }
         if (pow_hash(solution) == target) {
-            printf("Solution accepted: %08ld --> %08ld\n", target, solution);
-            confirmation = 0;
+            printf("Solution accepted: %08ld --> %08ld", target, solution);
+            printf("\n");
+            check = 0;
         } else {
-            printf("Solution rejected: %08ld !-> %08ld\n", target, solution);
-            confirmation = 1;
+            printf("Solution rejected: %08ld !-> %08ld", target, solution);
+            printf("\n");
+            check = 1;
         }
-        if (write(pipe_mon2m_write, &confirmation, sizeof(int)) != sizeof(int)) {
-            perror("Error escribiendo confirmación en monitor");
+        if (write(pipe2_write, &check, sizeof(int)) != sizeof(int)) {
+            perror("Error writing confirmation in monitor");
             exit(EXIT_FAILURE);
         }
     }
