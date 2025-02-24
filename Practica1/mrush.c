0b02fba4d25eb5e5bfeaa781663af4a7d3155e2e
pablopc3 <130652177+pablopc3@users.noreply.github.com>
2025-02-24 11:50:05 +0100

    Update mrush.c

--- Practica1/mrush.c
+++ Practica1/mrush.c
@@ -1,68 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include "pow.h"
 
-// Declaración de la función miner con la nueva firma.
-void miner(int rounds, int num_threads, long int initial_target,
-           int pipe_m2mon_read, int pipe_m2mon_write,
-           int pipe_mon2m_read, int pipe_mon2m_write);
+#define MAX_PIPE 2
+
+void miner(int rounds, int num_threads, long int target_ini, int pipe1_read, int pipe1_write, int pipe2_read, int pipe2_write);
+
+void validate_input(int argc, char *argv[], long int *target_ini, int *rounds, int *n_threads) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <TARGET_INI> <ROUNDS> <N_THREADS>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    *target_ini = atol(argv[1]);
+    *rounds = atoi(argv[2]);
+    *n_threads = atoi(argv[3]);
+
+    if (*rounds <= 0 || *n_threads <= 0) {
+        fprintf(stderr, "Rounds and/or n_threads need to be a positive number.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void create_pipes(int pipe1[2], int pipe2[2]) {
+
+    if (pipe(pipe1) == -1) {
+        perror("Error creating pipe1");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(pipe2) == -1) {
+        perror("Error creating pipe2");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Uso: %s <TARGET_INI> <ROUNDS> <N_THREADS>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    long int target = atol(argv[1]);
-    int rounds = atoi(argv[2]);
-    int num_threads = atoi(argv[3]);
 
-    // Se validan los paramteros introducidos
-    if (rounds <= 0 || num_threads <= 0) {
-        fprintf(stderr, "Rounds and number of threads must be positive integers.\n");
-        exit(EXIT_FAILURE);
-    }
+    long int target;
+    int rounds, n_threads;
+    int pipe1[MAX_PIPE], pipe2[MAX_PIPE];
+    pid_t miner_pid;
+    int status;
 
-    // Creamos dos pipes: uno para Minero->Monitor y otro para Monitor->Minero.
-    int pipe_m2mon[2];
-    int pipe_mon2m[2];
-    if (pipe(pipe_m2mon) == -1) {
-        perror("Error creando la tubería m2mon");
-        exit(EXIT_FAILURE);
-    }
-    if (pipe(pipe_mon2m) == -1) {
-        perror("Error creando la tubería mon2m");
-        exit(EXIT_FAILURE);
-    }
+    validate_input(argc, argv, &target, &rounds, &n_threads);
 
-    pid_t miner_pid = fork();
+    create_pipes(pipe1, pipe2);
+
+    miner_pid = fork();
     if (miner_pid < 0) {
-        perror("Error en fork()");
+        perror("Error in fork()");
         exit(EXIT_FAILURE);
     }
 
     if (miner_pid == 0) {
-        // En el proceso Minero, pasamos ambos extremos de cada pipe.
-        miner(rounds, num_threads, target,
-              pipe_m2mon[0], pipe_m2mon[1],
-              pipe_mon2m[0], pipe_mon2m[1]);
-        // Una vez finalizado, cerramos los descriptores heredados.
-        close(pipe_m2mon[0]);
-        close(pipe_m2mon[1]);
-        close(pipe_mon2m[0]);
-        close(pipe_mon2m[1]);
+        miner(rounds, n_threads, target, pipe1[0], pipe1[1], pipe2[0], pipe2[1]);
+        close(pipe1[0]);
+        close(pipe1[1]);
+        close(pipe2[0]);
+        close(pipe2[1]);
         exit(EXIT_SUCCESS);
     }
 
-    // El proceso Principal cierra sus copias de los pipes.
-    close(pipe_m2mon[0]);
-    close(pipe_m2mon[1]);
-    close(pipe_mon2m[0]);
-    close(pipe_mon2m[1]);
+    close(pipe1[0]);
+    close(pipe1[1]);
+    close(pipe2[0]);
+    close(pipe2[1]);
 
-    int status;
     waitpid(miner_pid, &status, 0);
     if (WIFEXITED(status))
         printf("Miner exited with status %d\n", WEXITSTATUS(status));
