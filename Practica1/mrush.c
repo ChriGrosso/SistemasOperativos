e6c2a32e9e15b8720361c1988a4e1d16a352bcbd
pablopc3 <130652177+pablopc3@users.noreply.github.com>
2025-02-24 12:53:09 +0100

    Update mrush.c

--- Practica1/mrush.c
+++ Practica1/mrush.c
@@ -1,3 +1,14 @@
+/**
+ * @file mrush.c
+ * @brief Función principal para el sistema de minado.
+ *
+ * Este archivo se encarga de validar los parámetros de entrada, crear los pipes necesarios,
+ * forkar el proceso Minero y esperar a su finalización.
+ *
+ * @author Christian y Pablo
+ * @date 2025-02-21
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,8 +18,32 @@
 
 #define MAX_PIPE 2
 
+/**
+ * @brief Declaración de la función miner.
+ *
+ * Implementada en miner.c.
+ *
+ * @param rounds Número de rondas de minado.
+ * @param num_threads Número de hilos a utilizar.
+ * @param target_ini Target inicial para el minado.
+ * @param pipe1_read Descriptor de lectura del primer pipe.
+ * @param pipe1_write Descriptor de escritura del primer pipe.
+ * @param pipe2_read Descriptor de lectura del segundo pipe.
+ * @param pipe2_write Descriptor de escritura del segundo pipe.
+ */
 void miner(int rounds, int num_threads, long int target_ini, int pipe1_read, int pipe1_write, int pipe2_read, int pipe2_write);
 
+/**
+ * @brief Valida los parámetros de entrada.
+ *
+ * Verifica que se hayan proporcionado los argumentos correctos y que rounds y n_threads sean positivos.
+ *
+ * @param argc Número de argumentos.
+ * @param argv Arreglo de argumentos.
+ * @param target_ini Puntero para almacenar el target inicial.
+ * @param rounds Puntero para almacenar el número de rondas.
+ * @param n_threads Puntero para almacenar el número de hilos.
+ */
 void validate_input(int argc, char *argv[], long int *target_ini, int *rounds, int *n_threads) {
     if (argc != 4) {
         fprintf(stderr, "Usage: %s <TARGET_INI> <ROUNDS> <N_THREADS>\n", argv[0]);
@@ -25,6 +60,12 @@ void validate_input(int argc, char *argv[], long int *target_ini, int *rounds, i
     }
 }
 
+/**
+ * @brief Crea dos pipes para la comunicación entre procesos.
+ *
+ * @param pipe1 Array de dos enteros para el primer pipe.
+ * @param pipe2 Array de dos enteros para el segundo pipe.
+ */
 void create_pipes(int pipe1[2], int pipe2[2]) {
 
     if (pipe(pipe1) == -1) {
@@ -37,6 +78,15 @@ void create_pipes(int pipe1[2], int pipe2[2]) {
     }
 }
 
+/**
+ * @brief Función principal.
+ *
+ * Parsea los argumentos, crea los pipes, forka el proceso Minero y espera a que éste finalice.
+ *
+ * @param argc Número de argumentos.
+ * @param argv Arreglo de argumentos.
+ * @return EXIT_SUCCESS si todo sale correctamente, EXIT_FAILURE en caso contrario.
+ */
 int main(int argc, char *argv[]) {
 
     long int target;
