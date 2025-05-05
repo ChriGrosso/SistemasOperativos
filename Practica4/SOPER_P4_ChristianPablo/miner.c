42263f0377f3b00d55555f90372fcd28c216b7d6
pablopc3 <130652177+pablopc3@users.noreply.github.com>
2025-05-05 16:09:00 +0200

    Update miner.c

--- Practica4/SOPER_P4_ChristianPablo/miner.c
+++ Practica4/SOPER_P4_ChristianPablo/miner.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/msg.h>
-#include <sys/shm.h>
+#include <mqueue.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <semaphore.h>
+#include <errno.h>
 #include "pow.h"
 
-#define MSG_KEY        0x3000
-#define SYS_KEY        0x1000
+#define MQ_NAME        "/block_queue"
+#define SHM_NAME       "/system_shm"
 #define MAX_MINERS     100
 #define TERMINATION_ID -1
 
@@ -18,11 +20,11 @@ typedef struct {
 } wallet_t;
 
 typedef struct {
-    sem_t      mutex;               // exclusión mutua
-    int        next_block_id;      // contador global de bloques
-    int        num_miners;         // mineros activos
-    pid_t      miners[MAX_MINERS]; // PIDs
-    int        coins[MAX_MINERS];  // monedas por minero
+    sem_t   mutex;
+    int     next_block_id;
+    int     num_miners;
+    pid_t   miners[MAX_MINERS];
+    int     coins[MAX_MINERS];
 } system_state_t;
 
 typedef struct {
@@ -36,10 +38,10 @@ typedef struct {
     wallet_t wallets[MAX_MINERS];
 } block_t;
 
-typedef struct {
-    long    mtype;
-    block_t blk;
-} msgbuf_t;
+// wrapper para sem_wait que reintenta tras EINTR
+static void sem_wait_eintr(sem_t *s) {
+    while (sem_wait(s) == -1 && errno == EINTR) { /* retry */ }
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -47,95 +49,78 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
     int rounds = atoi(argv[1]);
-    (void)argv; // threads no usado aquí
 
-    // 1) Conectar/inicializar shm sistema
-    int shmid = shmget(SYS_KEY, sizeof(system_state_t),
-                      IPC_CREAT | IPC_EXCL | 0666);
+    // 1) shm POSIX
+    int fd = shm_open(SHM_NAME, O_CREAT|O_EXCL|O_RDWR, 0666);
     system_state_t *sys;
-    if (shmid >= 0) {
-        sys = shmat(shmid, NULL, 0);
+    if (fd >= 0) {
+        ftruncate(fd, sizeof *sys);
+        sys = mmap(NULL, sizeof *sys, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
         sem_init(&sys->mutex, 1, 1);
         sys->next_block_id = 0;
         sys->num_miners    = 0;
     } else {
-        shmid = shmget(SYS_KEY, sizeof(system_state_t), 0666);
-        sys = shmat(shmid, NULL, 0);
+        fd = shm_open(SHM_NAME, O_RDWR, 0666);
+        sys = mmap(NULL, sizeof *sys, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
     }
 
-    // 2) Registrar este minero
-    sem_wait(&sys->mutex);
-    int my_idx = sys->num_miners;
-    sys->miners[my_idx] = getpid();
-    sys->coins[my_idx]  = 0;
+    // 2) registro
+    sem_wait_eintr(&sys->mutex);
+    int idx = sys->num_miners;
+    sys->miners[idx] = getpid();
+    sys->coins[idx]  = 0;
     sys->num_miners++;
     sem_post(&sys->mutex);
 
-    while(1){
-        sem_wait(&sys->mutex);
-        int cur = sys->num_miners;
-        sem_post(&sys->mutex);
-        if(cur >= 2) break;
-        usleep(100000);
-    }
-
-    // 3) Abrir cola de mensajes
-    int msgid = msgget(MSG_KEY, IPC_CREAT | 0666);
-    if (msgid < 0) { perror("msgget"); return EXIT_FAILURE; }
+    // 3) cola de mensajes POSIX
+    struct mq_attr attr = { .mq_flags = 0, .mq_maxmsg = 10, .mq_msgsize = sizeof(block_t) };
+    mqd_t mq = mq_open(MQ_NAME, O_CREAT|O_WRONLY, 0666, &attr);
+    if (mq == (mqd_t)-1) { perror("mq_open"); return EXIT_FAILURE; }
 
     printf("[%d] Generating blocks ...\n", getpid());
-
     long target = 0;
     for (int r = 0; r < rounds; ++r) {
-        // 4) Resolver POW
         long sol = pow_hash(target);
 
-        // 5) Bajo mutex: asignar ID, actualizar monedas y snapshot
-        sem_wait(&sys->mutex);
-        int id        = sys->next_block_id++;
-        int nm        = sys->num_miners;
-        sys->coins[my_idx]++;  // añadir moneda al ganador
-
-        block_t blk;
-        blk.id           = id;
-        blk.target       = target;
-        blk.solution     = sol;
-        blk.winner_pid   = getpid();
-        blk.votes_yes    = nm;
-        blk.votes_total  = nm;
-        blk.num_wallets  = nm;
+        sem_wait_eintr(&sys->mutex);
+        int id = sys->next_block_id++;
+        sys->coins[idx]++;
+        int nm = sys->num_miners;
+        block_t blk = {
+            .id           = id,
+            .target       = target,
+            .solution     = sol,
+            .winner_pid   = getpid(),
+            .votes_yes    = nm,
+            .votes_total  = nm,
+            .num_wallets  = nm
+        };
         for (int i = 0; i < nm; ++i) {
             blk.wallets[i].pid   = sys->miners[i];
             blk.wallets[i].coins = sys->coins[i];
         }
         sem_post(&sys->mutex);
 
-        // 6) Enviar bloque al monitor
-        msgbuf_t msg = { .mtype = 1, .blk = blk };
-        if (msgsnd(msgid, &msg, sizeof(block_t), 0) < 0)
-            perror("msgsnd");
-
+        mq_send(mq, (char*)&blk, sizeof blk, 0);
         target = sol;
         usleep(100000);
     }
 
     printf("[%d] Finishing\n", getpid());
 
-    // 7) Desregistrar y, si soy el último, enviar terminación
-    sem_wait(&sys->mutex);
+    sem_wait_eintr(&sys->mutex);
     sys->num_miners--;
     int last = (sys->num_miners == 0);
     sem_post(&sys->mutex);
 
     if (last) {
         block_t endb = { .id = TERMINATION_ID };
-        msgbuf_t endmsg = { .mtype = 1, .blk = endb };
-        msgsnd(msgid, &endmsg, sizeof(block_t), 0);
-        msgctl(msgid, IPC_RMID, NULL);
-        shmdt(sys);
-        shmctl(shmid, IPC_RMID, NULL);
+        mq_send(mq, (char*)&endb, sizeof endb, 0);
+        mq_close(mq); mq_unlink(MQ_NAME);
+        munmap(sys, sizeof *sys); shm_unlink(SHM_NAME);
     } else {
-        shmdt(sys);
+        mq_close(mq);
+        munmap(sys, sizeof *sys);
     }
 
     return EXIT_SUCCESS;
